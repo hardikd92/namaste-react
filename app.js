2e81b6b60390d28c0ab6fbbff5224b95e667717d
hardik bhatia <bhatia.hardik@yahoo.com>
2024-01-16 21:59:15 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,13 @@
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const heading = React.createElement("h1", {}, "I'm h1 tag");
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
+root.render(parent);
