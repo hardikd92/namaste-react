0d5f576626eb0d46a796d37ce367da859867220f
hardik bhatia <bhatia.hardik@yahoo.com>
2024-01-24 10:00:35 +0530

    till now

--- app.js
+++ app.js
@@ -49,30 +49,25 @@ const RestaurantCards = () => {
   return (
     <div className="card" style={{ width: "18rem" }}>
       <img
-        width="50"
+        width="70"
         height="70"
         src="https://img.icons8.com/matisse/100/hamburger.png"
         className="card-img-top"
         alt="..."
+        style={{ width: "85px", margin: "0px auto" }}
       />
       <div className="card-body">
         <h5 className="card-title">Burger King</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
+        <p className="card-text"></p>
       </div>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
+        <li className="list-group-item">Burger, cafe</li>
+        <li className="list-group-item">20 Min</li>
+        <li className="list-group-item">4.4 Star</li>
       </ul>
       <div className="card-body">
         <a href="#" className="card-link">
-          Card link
-        </a>
-        <a href="#" className="card-link">
-          Another link
+          Order Now
         </a>
       </div>
     </div>
@@ -80,7 +75,12 @@ const RestaurantCards = () => {
 };
 const BodyComponent = () => {
   return (
-    <div className="body-area">
+    <div className="body-area" style={{ display: "flex" }}>
+      <RestaurantCards />
+      <RestaurantCards />
+      <RestaurantCards />
+      <RestaurantCards />
+      <RestaurantCards />
       <RestaurantCards />
     </div>
   );
