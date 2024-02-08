b329522ed67b3f59aa537547feea101f0d9b8305
hardik bhatia <bhatia.hardik@yahoo.com>
2024-02-08 21:48:05 +0530

    search added for restros

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -6,6 +6,9 @@ import Shimmer from "./layout/Shimmer";
 
 const BodyComponent = () => {
   const [restrosList, setRestroList] = useState([]);
+  const [filteredRestrosList, setfilteredRestrosList] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
   useEffect(() => {
     fetchApiData();
   }, []);
@@ -15,6 +18,9 @@ const BodyComponent = () => {
     const json = await data.json();
     console.log(json);
     setRestroList(
+      json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+    setfilteredRestrosList(
       json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
     );
   };
@@ -28,59 +34,91 @@ const BodyComponent = () => {
     </div>
   ) : (
     <div className="body-area">
-      <div className="filterArea">
-        {/*Top rated Restaurant filter button*/}
-        <button
-          className="btn btn-primary  m-1"
-          type="button"
-          onClick={() => {
-            const filterRestorsList = restrosList.filter(
-              (restro) => restro?.info?.avgRating > 4
-            );
-            setRestroList(filterRestorsList);
-          }}
-        >
-          Top Rated Restaurant
-        </button>
-        {/* Lowest Delivery Time Filter Button */}
-        <button
-          className="btn btn-primary  m-1"
-          type="button"
-          onClick={() => {
-            const filterRestorsList = restrosList.filter(
-              (restro) => restro?.info?.sla?.deliveryTime < 20
-            );
-            setRestroList(filterRestorsList);
-          }}
-        >
-          Lowest Delivery Time
-        </button>
-        {/* Only Veg Filter Button */}
-        <button
-          className="btn btn-success  m-1"
-          type="button"
-          onClick={() => {
-            const filterRestorsList = restrosList.filter(
-              (restro) => restro?.info?.veg == true
-            );
-            setRestroList(filterRestorsList);
-          }}
-        >
-          Only Veg
-        </button>
-        {/* Reset Filter Button */}
-        <button
-          className="btn btn-warning  m-1"
-          type="button"
-          onClick={() => {
-            setRestroList(restroList);
-          }}
-        >
-          Reset Filter
-        </button>
+      <div className="filterArea d-flex">
+        <div className="filter-area">
+          {/*Top rated Restaurant filter button*/}
+          <button
+            className="btn btn-primary  m-1"
+            type="button"
+            onClick={() => {
+              const filterRestorsList = restrosList.filter(
+                (restro) => restro?.info?.avgRating > 4
+              );
+              setfilteredRestrosList(filterRestorsList);
+            }}
+          >
+            Top Rated Restaurant
+          </button>
+          {/* Lowest Delivery Time Filter Button */}
+          <button
+            className="btn btn-primary  m-1"
+            type="button"
+            onClick={() => {
+              const filterRestorsList = restrosList.filter(
+                (restro) => restro?.info?.sla?.deliveryTime < 20
+              );
+              setfilteredRestrosList(filterRestorsList);
+            }}
+          >
+            Lowest Delivery Time
+          </button>
+          {/* Only Veg Filter Button */}
+          <button
+            className="btn btn-success  m-1"
+            type="button"
+            onClick={() => {
+              const filterRestorsList = restrosList.filter(
+                (restro) => restro?.info?.veg == true
+              );
+              setfilteredRestrosList(filterRestorsList);
+            }}
+          >
+            Only Veg
+          </button>
+          {/* Reset Filter Button */}
+          <button
+            className="btn btn-warning  m-1"
+            type="button"
+            onClick={() => {
+              setfilteredRestrosList(restroList);
+            }}
+          >
+            Reset Filter
+          </button>
+        </div>
+        <div className="search-area">
+          <div className="input-group m-1 mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search Restaurant"
+              aria-label="Restaurant name"
+              aria-describedby="button-addon2"
+              value={searchValue}
+              onChange={(e) => {
+                setSearchValue(e.target.value);
+              }}
+            />
+            <button
+              className="btn btn-outline-secondary  mr-2"
+              type="button"
+              id="button-addon2"
+              onClick={() => {
+                const filteredRestroList = restrosList.filter((restro) =>
+                  restro?.info?.name
+                    .toLowerCase()
+                    .includes(searchValue.toLowerCase())
+                );
+                setfilteredRestrosList(filteredRestroList);
+              }}
+            >
+              Search
+            </button>
+          </div>
+        </div>
       </div>
       <div className="restaurant-cards row">
-        {restrosList.map((restro) => (
+        {filteredRestrosList.map((restro) => (
           <RestaurantCards key={restro?.id?.id} restroData={restro} />
         ))}
       </div>
